9258dd7ba3d62c787ff5129db2c8ad933f730aa2
CNshilpa <shilpa.cnv@gmail.com>
2022-07-03 13:16:59 +0530

    addition : CompareTo Methods

--- src/DistanceBwPoint.java
+++ src/DistanceBwPoint.java
@@ -6,22 +6,25 @@ class DistanceBwPoint {
 
 	{
 
-		int x1, x2, y1, y2, x3, x4, y3, y4;
+		int y1, y2, x3, x4, y3, y4;
 		double dis;
-
+		@SuppressWarnings("removal")
+		Integer x1 = new Integer(10);
+		@SuppressWarnings("removal")
+		Integer x2 = new Integer(10);
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter x1 point");
+		// System.out.println("Enter x1 point");
 
-		x1 = sc.nextInt();
+		// x1 = sc.nextInt();
 
 		System.out.println("Enter y1 point");
 
 		y1 = sc.nextInt();
 
-		System.out.println("Enter x2 point");
+		// System.out.println("Enter x2 point");
 
-		x2 = sc.nextInt();
+		// x2 = sc.nextInt();
 		System.out.println("Enter y2 point");
 
 		y2 = sc.nextInt();
@@ -41,7 +44,8 @@ class DistanceBwPoint {
 
 		dis = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 		System.out.println("Welcome to Line\r\n" + "Comparison Computation\r\n" + "Program on Master Branch");
-		System.out.println("Length of a line between" + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ")===>" + dis);
+		System.out
+				.println("Length of a line between" + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ")===>" + dis);
 		double length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 
 		double length2 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
@@ -51,5 +55,6 @@ class DistanceBwPoint {
 			System.out.println(length1 + " Length1 is the Greater");
 		else
 			System.out.println(length2 + " Length2 is the Greater");
+		System.out.println(x1.compareTo(x2));
 	}
 }
